b28954d5c3d0246bb422f4620220b3691cd5f984
Gregory Thompson <mrgregorythompsonii@gmail.com>
2021-04-20 15:09:24 -0500

    users can now create new Tags

--- Tabloid/client/src/components/Tags/TagForm.js
+++ Tabloid/client/src/components/Tags/TagForm.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import { TagContext } from "../../providers/TagProvider";
 import { useHistory } from "react-router-dom";
 import { Link } from 'react-router-dom';
-import { Button } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
 import "./Tags.css";
 
 
@@ -10,7 +10,7 @@ export const TagForm = () => {
     const { addTag } = useContext(TagContext);
 
     const [tag, setTags] = useState({
-        name: ""
+        Name: ""
     });
 
     const history = useHistory();
@@ -18,26 +18,32 @@ export const TagForm = () => {
     const handleControlledInputChange = (event) => {
         const newTag = { ...tag };
         let selectedVal = event.target.value;
+        //The event.target.id is "name" (the form Input Id)
         newTag[event.target.id] = selectedVal
         setTags(newTag);
     }
 
-    const handleClickSaveLongRun = (event) => {
+    const handleClickSaveTag = (event) => {
         event.preventDefault();
-
-        const name = tag.name;
-        if (name === "") {
+        if (tag.Name === "") {
             window.alert("Please provide a title for the tag you are trying to create.");
         } else {
             addTag(tag)
-                .then(() => history.push("/tags"));
+                .then(() => history.push("/tag"));
         }
     }
 
     return (
         <>
             <div>
-                {/* Add the tag form JSX here */}
+                <Form className="addTagDiv" onSubmit={handleClickSaveTag}>
+                    <Label for="tagInput">New Tag Name</Label>
+                    <Input id="Name"
+                        placeholder="Enter Tag Name"
+                        type="text"
+                        onChange={handleControlledInputChange}></Input>
+                    <Button className="a">Save</Button>
+                </Form>
             </div>
         </>
     )
