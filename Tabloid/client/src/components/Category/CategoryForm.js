ce13b795cdca4f07d5711c0bba8bf77a233c368a
Jess Dever <jessicadever1@gmail.com>
2021-04-20 10:51:44 -0500

    working on form saving

--- Tabloid/client/src/components/Category/CategoryForm.js
+++ Tabloid/client/src/components/Category/CategoryForm.js
@@ -1,24 +1,34 @@
 import React, { useContext, useState } from "react";
 import { CategoryContext } from "../../providers/CategoryProvider"
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
 
 export const CategoryForm = () => {
 
     const { addCategory } = useContext(CategoryContext)
+    const history = useHistory();
 
     const [category, setCategory] = useState({
         "id": 0,
         "Name": ""
     })
 
+    const handleClickSaveCat = (event) => {
+        event.preventDefault()
 
+        addCategory({
+            id: category.id,
+            name: category.name
+        })
+            .then(() => history.push(`/category`))
+    }
 
     return (
-        <div className="addCatDiv">
-            <label>New Category Name</label>
-            <input id="catInput" placeholder="Enter Category Name"></input>
-            <button><Link to="/category">Save</Link></button>
-        </div>
+        <Form className="addCatDiv" onSubmit={handleClickSaveCat}>
+            <Label>New Category Name</Label>
+            <Input id="catInput" placeholder="Enter Category Name"></Input>
+            <Button>Save</Button>
+        </Form>
 
     )
 
