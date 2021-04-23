1c09c9a585996769dd6bafc7a1efcec5e3123b51
Jess Dever <jessicadever1@gmail.com>
2021-04-23 11:38:31 -0500

    controller update

--- Tabloid/Controllers/CommentsController.cs
+++ Tabloid/Controllers/CommentsController.cs
@@ -1,15 +1,31 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tabloid.Repositories;
 
 namespace Tabloid.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CommentsController : ControllerBase
+    [Authorize]
+    public class CommentController : ControllerBase
     {
+        private readonly ICommentRepository _commentRepository;
+
+        public CommentController(ICommentRepository commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult Get(int id)
+        {
+            return Ok(_commentRepository.GetAllCommentsByPostId(id));
+        }
     }
 }
