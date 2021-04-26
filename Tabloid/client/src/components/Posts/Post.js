29c04aba6d89c3b2237cb7147e9fea664d341dfa
Xine <cjdoza@gmail.com>
2021-04-26 09:21:54 -0500

    post edit and delete done yay

--- Tabloid/client/src/components/Posts/Post.js
+++ Tabloid/client/src/components/Posts/Post.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, CardImg, CardBody, Button } from "reactstrap";
 import { Link } from "react-router-dom";
+import "./Posts.css";
 
 export const Post = ({ post }) => {
 
